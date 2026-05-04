41d5027dcc64e6d6b422f7419b161505133976ab
Amul Sinfal <amul.sinfal@gmail.com>
2026-05-04 12:12:08 +0530

    Add UnaryPlusMinus.java file.

---
+++ src/operators/UnaryPlusMinus.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class UnaryPlusMinus {
+	public static void main(String[] args) {
+		int i1 = +10;
+		int i2 = 10;
+		int i3 = -110;
+		int i4 = -(i1 + i2);
+
+		System.out.println("i1 = " + i1);
+		System.out.println("i2 = " + i2);
+		System.out.println("i3 = " + i3);
+		System.out.println("i4 = " + i4);
+	}
+}
