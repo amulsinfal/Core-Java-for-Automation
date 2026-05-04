4ef07ac35e3bff3b6e146bed6c2f927f497de8a7
Amul Sinfal <amul.sinfal@gmail.com>
2026-05-04 12:11:05 +0530

    Add Mulitplication.java file.

---
+++ src/operators/Multiplication.java
@@ -0,0 +1,20 @@
+package operators;
+
+public class Multiplication {
+	
+	public static void main(String[] args) {
+		int i1 = 5;
+		int i2 = 10;
+		float f1 = 15.5f;
+		double d1 = 20.5;
+		
+		int res1 = i1 * i2; // result will be an int
+		float res2 = i2 * f1; // result will be a float.
+		double res3 = d1 * i1; // result will be a double.
+		
+		System.out.println("res1 = " + res1);
+		System.out.println("res2 = " + res2);
+		System.out.println("res3 = " + res3);
+	}
+
+}
