588ea41496352aa8fd298378f66156e2767b0f78
Amul Sinfal <amul.sinfal@gmail.com>
2026-05-04 12:10:32 +0530

    Add Division.java file.

---
+++ src/operators/Division.java
@@ -0,0 +1,20 @@
+package operators;
+
+public class Division {
+
+	public static void main(String[] args) {
+		int i1 = 5;
+		int i2 = 10;
+		float f1 = 15.5f;
+		double d1 = 20.5;
+		
+		int res1 = i2 / i1; // result will be int
+		float res2 = f1 / i1; // result will be float
+		double res3 = d1 / i2; // result will be double.
+		
+		System.out.println("res1 = " + res1); 
+		System.out.println("res2 = " + res2);
+		System.out.println("res3 = " + res3);
+		
+	}
+}
