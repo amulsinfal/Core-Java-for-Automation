73894211542c14a8a6aaea6784bc0e01ec8b029c
Amul Sinfal <amul.sinfal@gmail.com>
2026-05-04 12:12:29 +0530

    Add LogicalComplement.java file.

---
+++ src/operators/LogicalComplement.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class LogicalComplement {
+
+	public static void main(String[] args) {
+		
+		boolean flag = true;
+		
+		System.out.println("flag = " + flag);
+		System.out.println("!flag = " + !flag);
+		System.out.println("!!flag = " + !!flag);
+	}
+}
